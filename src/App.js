9e8223e0e7272f5259e6fe835663c453e38e59eb
unbeknownst6k9 <46736851+unbeknownst6k9@users.noreply.github.com>
2022-02-23 21:09:09 -0500

    add new package and test buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
 import logo from './logo.svg';
 import './App.css';
 
+const BlueBtn = styled('button')({
+    color: 'darkslategray',
+    backgroundColor: 'aliceblue',
+    padding: 8,
+    borderRadius: 4,
+});
+
 function App() {
   return (
-    <div className="App">
+      <div className="App">
+          <BlueBtn>Hello Button</BlueBtn>
+          <Button sx={{
+              color: 'black',
+              fontWeight: 'medium',
+              mx: 0.5,
+          }}>material button</Button>
         <h1>Hello World</h1>
     </div>
   );
